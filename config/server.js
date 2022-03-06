1470c2b03aa965853004bd8368f668472ef0e31e
ismaelash <contato@ismaelnascimento.com>
2022-03-06 18:38:23 -0300

    initial commmit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1eb67afede5a33521798253814a37f04'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'df0277ae-1bd6-45c6-8880-adb36b11ccf3'
+};
